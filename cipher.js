54fa1b3a6ede862d410824449234fc18f32aa99b
Boris Kalekanov <b.e.kalekanov@gmail.com>
2023-02-05 11:43:29 +0200

    test

---
+++ cipher.js
@@ -0,0 +1,15 @@
+const msg = 'Hi mom';
+console.log(msg)
+const ciphereMsg = cipher ( msg)
+console.log(ciphereMsg)
+
+
+function cipher(msg){
+for (let index = 0; index < msg.length; index++) {
+    const element = msg[index];
+    console.log(element)
+
+}
+return '';
+
+}
